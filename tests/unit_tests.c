a61d3e28224828d26a615fd798af0d4388cb75bb
Enzo Fokwen <enzo.fokwen-rocher@epitech.eu>
2024-01-25 11:20:16 +0100

    [ADD] more unit_test

--- tests/unit_tests.c
+++ tests/unit_tests.c
@@ -1,12 +1,11 @@
 /*
 ** EPITECH PROJECT, 2023
-** MY_RADAR
+** MY_NAVY
 ** File description:
 ** unit tests
 */
 
 #include <criterion/criterion.h>
-#include <SFML/Graphics.h>
 #include "../include/navy_func.h"
 
 /*
@@ -38,14 +37,14 @@ Test(unit_test, wrong_file)
     cr_assert_eq(err_handling(3, args2), ERROR);
 }
 
-/*Test(unit_test, good_file)
+Test(unit_test, good_file)
 {
-    char *args[] = {"./my_navy", "pos1"};
-    char *args2[] = {"./my_navy", "4586", "pos1"};
+    char *args[] = {"./my_navy", "tests/pos1"};
+    char *args2[] = {"./my_navy", "4586", "tests/pos1"};
 
-    cr_assert_eq(err_handling(2, args), 0);
-    cr_assert_eq(err_handling(3, args2), 0);
-}*/
+    cr_assert_eq(err_handling(2, args), SUCCESS);
+    cr_assert_eq(err_handling(3, args2), SUCCESS);
+}
 
 Test(unit_test, losing_condition)
 {
@@ -69,11 +68,26 @@ Test(unit_test, get_coords_from_line)
     cr_assert_str_eq(get_coord(line + 3), "E3");
 }
 
+Test(unit_test, int_to_str_bin)
+{
+    int nb1 = 0;
+    int nb2 = 789;
+
+    cr_assert_str_eq(int_to_bin(nb1), "10000000000000000000000000000000");
+    cr_assert_str_eq(int_to_bin(nb2), "10000000000000000000001100010101");
+}
+
 /*----- FUNCTIONNAL TEST ----*/
 
 Test(functionnal_test, err_handling)
 {
     char *args[] = {"./my_navy"};
+    char *args3[] = {"./my_navy", "pop"};
+    char *args2[] = {"./my_navy", "4586", "not_a_file"};
+    char *args4[] = {"./my_navy", "1234", "figh", "feogho"};
 
+    cr_assert_eq(my_navy(2, args3), ERROR);
+    cr_assert_eq(my_navy(3, args2), ERROR);
     cr_assert_eq(my_navy(1, args), ERROR);
+    cr_assert_eq(err_handling(4,  args2), ERROR);
 }
